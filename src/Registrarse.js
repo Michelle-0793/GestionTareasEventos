491e7e645269d9f00c86eda5c78de9cff240c720
Michelle-0793 <mlopez@fwdcostarica.com>
2024-07-24 10:22:06 -0600

    Añadí el input de nombre completo

--- src/Registrarse.js
+++ src/Registrarse.js
@@ -1,11 +1,11 @@
-
+const nombre = document.getElementById("nombre")
 const usuario=document.getElementById("Usuario");
 const correo=document.getElementById("correoElectrónico");
 const contraseña=document.getElementById("contraseña");
 const boton=document.getElementById("btnRegistrarse");
 
 
-
+let nombre1=nombre.value
 let usuario1=usuario.value;
 let correo1=correo.value;
 let contraseña1=contraseña.value;
@@ -17,6 +17,7 @@ boton.addEventListener("click", function () {
 
 
     let persona={
+        Nombre:nombre.value,
         Usuario:usuario.value,
         correoElectrónico:correo.value,
         Contraseña:contraseña.value,
@@ -28,6 +29,7 @@ lista.push(persona);
 
 localStorage.setItem("Personas registradas", JSON.stringify(lista))
 
+nombre.value="";
 usuario.value="";
 correo.value="";
 contraseña.value="";
