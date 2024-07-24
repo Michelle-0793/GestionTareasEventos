67d0c8ed95cfab9f25232608c5386772e6e86d88
Michelle-0793 <mlopez@fwdcostarica.com>
2024-07-24 14:00:59 -0600

    se logró con éxito hacer la validación en iniciar sesión

--- src/iniciarSesión.js
+++ src/iniciarSesión.js
@@ -1,25 +1,19 @@
-const usuario=document.getElementById("usuario");
-const contraseña=document.getElementById("contraseña");
-const usuarioRegistrado=document.getElementById("usuarioRegistrado");
-const contraseñaRegistrada=document.getElementById("contraseñaRegistrada");
-const botonIngresar=document.getElementById("ingresar");
-
-
+let lista= JSON.parse(localStorage.getItem ("personasRegistradas"));
+const usuario = document.getElementById("usuarioRegistrado");
+const contraseña = document.getElementById("contraseñaRegistrada");
+const botonIngresar = document.getElementById("ingresar");
 
 
 botonIngresar.addEventListener("click", function () {
+for (let index = 0; index < lista.length; index++) { 
+    
+  let usuarioRegistrado=lista[index].Usuario;
+  let contraseñaRegistrada=lista[index].Contraseña;
 
-let lista= JSON.parse(localStorage.getItem ("personasRegistradas")) || [];
-
-  for (let index = 0; index < lista.length; index++) { 
-  
-  let usuarioIngresado=lista[index].usuario
-  let contraseñaIngresada=lista[index].contraseña
-
-  if (usuarioIngresado==usuario.value  && contraseñaIngresada==contraseña.value) {
+  if (usuarioRegistrado==usuario.value  && contraseñaRegistrada==contraseña.value) {
       alert ("¡Bienvenido!");
   }else{
+
     alert ("No está registrado");
   }
-  } 
-})
+  }})
