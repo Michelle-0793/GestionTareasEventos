84647fc263d41953b242bbab3a56ffa368d539a8
Michelle-0793 <mlopez@fwdcostarica.com>
2024-07-25 15:47:51 -0600

    modicación de evento al java de administración

--- src/administración.js
+++ src/administración.js
@@ -4,28 +4,26 @@ const fechaInput = document.getElementById("fechaInput");
 
 const btnGuardar = document.getElementById("btnGuardar");
 
-const select=document.getElementById("select")
+const select = document.getElementById("select")
 
 const cajaTareas = document.getElementById("cajaTareas");
 
 const cajaEventos = document.getElementById("cajaEventos");
 
 
-
 btnGuardar.addEventListener("click", function () {
-const sumaTareasEventos = inputTareaEvento.value+ " " +fechaInput.value;
-const tipo = select.value;
-etiquetaP = document.createElement("p");
+const titulo = inputTareaEvento.value;
+const fecha = fechaInput.value;
+const tipo = select.value; 
 
-e
+let etiquetaP = document.createElement("p");
 
-
-if (tipo === "tarea") {
+if (tipo==="tarea") {
     cajaTareas.appendChild(etiquetaP);
 }
 
-if (tipo ==="evento") {
-    cajaEventos.appendChild(etiquetaP);
+if (tipo.value==="evento") {
+cajaEventos.appendChild(etiquetaP)
 }
 
 })
