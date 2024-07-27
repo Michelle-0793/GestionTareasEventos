141d5dcf118be6dc0ad3e08cc1d85540f7e3f9b4
Michelle-0793 <mlopez@fwdcostarica.com>
2024-07-27 00:48:04 -0600

    funcionalidad de agregar y editar eventos lista

--- src/administración.js
+++ src/administración.js
@@ -11,10 +11,11 @@ const texto = inputTareaEvento.value;
 const fecha = fechaInput.value;
 const tipo = select.value; 
 
-if (tipo==="tarea" && texto === "") {
-    alert ("Debe ingresar una tarea")
+if (tipo==="tarea") {
+    if (texto === "") {
+    alert ("Debe ingresar una tarea")  
     
-    }else{
+}else{
     const etiquetaP = document.createElement("p");
     const btnEliminar = document.createElement("button");
     const btnEditar = document.createElement("button");
@@ -29,55 +30,77 @@ if (tipo==="tarea" && texto === "") {
     cajaTareas.appendChild(btnEditar);
 
 btnEliminar.addEventListener("click", function () {
-    etiquetaP.remove();
-    btnEliminar.remove()
-    btnEditar.remove();
-    alert ("Se eliminó correctamente");
-})
+     etiquetaP.remove();
+     btnEliminar.remove()
+     btnEditar.remove();
+     alert ("Se eliminó correctamente");
+});
 
 btnEditar.addEventListener("click", function () {
-    const inputEdit = document.createElement("input");
-    const btnSave = document.createElement("button");
+       const inputEdit = document.createElement("input");
+       const btnSave = document.createElement("button");
 
     inputEdit.value = texto;
+    etiquetaP.innerHTML ="";
     etiquetaP.appendChild(inputEdit);
 
     btnSave.innerHTML = "Guardar";
     etiquetaP.appendChild(btnSave);
 
     btnSave.addEventListener("click", function () {
-      etiquetaP.innerHTML = inputEdit.value;
-        
+        etiquetaP.innerHTML = inputEdit.value;  
     });
 });
+
 }
 
-if (tipo==="evento" && texto === ""  || fecha === "") {
-    alert("Debe ingresar un titulo y fecha para el evento")
+}else{ 
 
-}else{
+    if (texto === "" || fecha ==="") {
+        alert ("Debe ingresar un título y fecha para el evento") 
+    } else {
     const etiquetaP = document.createElement("p");
     const btnEliminar = document.createElement("button");
     const btnEditar = document.createElement("button");
 
+
     etiquetaP.innerHTML = texto+ " " +fecha;
-    cajaEventos.appendChild(etiquetaP)
+    cajaEventos.appendChild(etiquetaP);
 
     btnEliminar.innerHTML = "Eliminar";
     cajaEventos.appendChild(btnEliminar);
 
     btnEditar.innerHTML = "Editar";
-    cajaE.appendChild(btnEditar);
+    cajaEventos.appendChild(btnEditar);
 
 btnEliminar.addEventListener("click", function () {
-    etiquetaP.remove();
-    btnEliminar.remove()
-    btnEditar.remove();
-    alert ("Se eliminó correctamente");
-})
+        etiquetaP.remove();
+        btnEliminar.remove();
+        btnEditar.remove();
+        alert ("Se eliminó correctamente");
+    });
 
+btnEditar.addEventListener("click", function () {
+    const inputEdit = document.createElement("input");
+    const inputEditFecha = document.createElement("input");
+    const btnSave = document.createElement("button");
 
+    inputEdit.value = texto;
+    inputEditFecha.value = fecha;
+    etiquetaP.innerHTML ="";
+    etiquetaP.appendChild(inputEdit);
+    etiquetaP.appendChild(inputEditFecha);
+
+btnSave.innerHTML = "Guardar";
+    etiquetaP.appendChild(btnSave);
+
+btnSave.addEventListener("click", function () {
+    etiquetaP.innerHTML = inputEdit.value+ " " + inputEditFecha.value;
+   });
+});
 
 }
-})
+}
+
+});
   
