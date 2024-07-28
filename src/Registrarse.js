cbe9af8de19c57f0baa236d46afc3152a58e2b61
Michelle-0793 <mlopez@fwdcostarica.com>
2024-07-28 03:10:32 -0600

    Se valida que un usuario ya está registrado, se dio 90% de estilo al HTML de registrarse

--- src/Registrarse.js
+++ src/Registrarse.js
@@ -15,16 +15,32 @@ boton.addEventListener("click", function () {
         Usuario:usuario.value,
         correoElectrónico:correo.value,
         Contraseña:contraseña.value,
-    }    
+    };   
 
-lista.push(persona);
-
-localStorage.setItem("personasRegistradas", JSON.stringify(lista))
-
+if (nombre.value==="" || usuario.value==="" || correo.value==="" || contraseña.value==="") {
+    alert("Debe completar todos los campos")
+} else {
+    let usuarioYaRegistrado=false
+    for (let index = 0; index < lista.length; index++) {
+        if (lista[index].Usuario === usuario.value) {
+            usuarioYaRegistrado=true;
+            break;
+        }
+}
 nombre.value="";
 usuario.value="";
 correo.value="";
 contraseña.value="";
 
+if (usuarioYaRegistrado) {
+    alert("Este usuario ya está registrado");
+}else{
+lista.push(persona);
+
+localStorage.setItem("personasRegistradas", JSON.stringify(lista))
+
+alert ("¡Se ha registrado con éxito!")
 window.location.href="iniciarSesión.html"
-})
+
+}
+}})
