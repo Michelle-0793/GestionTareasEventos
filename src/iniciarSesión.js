71944b7ad50e0a205ccc3810d8410ec1d5e72cd9
Michelle-0793 <mlopez@fwdcostarica.com>
2024-07-28 19:06:11 -0600

    corrección de la lógica de verficación dentro del for

--- src/iniciarSesión.js
+++ src/iniciarSesión.js
@@ -1,4 +1,4 @@
-let lista=JSON.parse(localStorage.getItem ("personasRegistradas")||[]);
+let lista=JSON.parse(localStorage.getItem ("personasRegistradas") || []);
 
 const usuario = document.getElementById("usuarioRegistrado");
 const contraseña = document.getElementById("contraseñaRegistrada");
@@ -6,19 +6,27 @@ const botonIngresar = document.getElementById("ingresar");
 
 
 botonIngresar.addEventListener("click", function () {
+let autenticado = false;
 
 for (let index = 0; index < lista.length; index++) { 
     
   let usuarioRegistrado=lista[index].Usuario;
   let contraseñaRegistrada=lista[index].Contraseña;
 
-  if (usuarioRegistrado==usuario.value  && contraseñaRegistrada==contraseña.value) {
+  
 
-    window.location.href="administración.html"
+  if (usuarioRegistrado==usuario.value  && contraseñaRegistrada==contraseña.value) {
+    autenticado = true;
+    window.location.href="administración.html";
+    break;
     
-  }else{
-    usuario.value="";
-    contraseña.value="";
+  }
   }
 
-  }})
+  if (!autenticado) {
+    usuario.value = "";
+    contraseña.value = "";
+  
+}
+alert("Usuario o contraseña incorrecto")
+});
